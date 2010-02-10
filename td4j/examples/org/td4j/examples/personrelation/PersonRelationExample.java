2fc28514504cb4cedb36b0b03f0a25699deb4e25
miraman <mchlrch@users.noreply.github.com>
2010-02-10 20:51:27 +0000

    rename

--- td4j/examples/org/td4j/examples/personrelation/PersonRelationExample.java
+++ td4j/examples/org/td4j/examples/personrelation/PersonRelationExample.java
@@ -15,9 +15,9 @@
 
   You should have received a copy of the GNU General Public License
   along with td4j.  If not, see <http://www.gnu.org/licenses/>.
-*********************************************************************/
+ *********************************************************************/
 
-package org.td4j.examples.personmultiaddress;
+package org.td4j.examples.personrelation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,57 +27,38 @@ import org.td4j.core.reflect.ExposeProperties;
 import org.td4j.core.reflect.ExposePropertiesInEditorList;
 import org.td4j.swing.workbench.Workbench;
 
-
 /**
- * This example shows the usage of the annotations  
- * @ExposePropertiesInEditorList and @ExposeProperties to configure
- * the properties shown in a table. 
+ * This example shows the usage of the annotations @ExposePropertiesInEditorList and
+ * @ExposeProperties to configure the properties shown in a table.
  */
-public class PersonMultiAddressExample {
+public class PersonRelationExample {
 
 	public static void main(String[] args) {
 		final Person homer = new Person("Homer", "Simpson");
-		homer.addAddress("742 Evergreen Terrace", "99001", "Springfield");
-
 		final Person bart = new Person("Bart", "Simpson");
 		final PersonRelation homerToBart = new PersonRelation(homer, bart, "is-father-of", "is-son-of");
-		homer.relationsOut.add(homerToBart);
-		bart.relationsIn.add(homerToBart);
-		
-		Workbench.start(homer, Person.class, Address.class);
-	}
 
+		Workbench.start(homer, Person.class);
+	}
 
 	// --------------------------------------
 
-	@ExposePropertiesInEditorList({"firstName", "lastName"})
+	@ExposePropertiesInEditorList( { "firstName", "lastName" })
 	public static class Person {
 
-	  public int age = 30;
-      public String firstName;
-      public String lastName;
-      
-      @ExposeProperties({"street", "zip", "city"})
-// TODO     @HideProperties({"person"})
-      public List<Address> addresses = new ArrayList<Address>();
-      
-      @ExposeProperties({"humanReadableForward", "forwardRole", "to"})
-      public List<PersonRelation> relationsOut = new ArrayList<PersonRelation>();
-      
-      @ExposeProperties({"humanReadableBackward", "backwardRole", "from"})
-      public List<PersonRelation> relationsIn = new ArrayList<PersonRelation>();
+		public String firstName;
+		public String lastName;
+
+		@ExposeProperties( { "forwardRole", "to" })
+		public List<PersonRelation> relationsOut = new ArrayList<PersonRelation>();
+
+		@ExposeProperties( { "backwardRole", "from" })
+		public List<PersonRelation> relationsIn = new ArrayList<PersonRelation>();
 
 		@Executable(paramNames = { "firstName", "lastName" })
 		public Person(String firstName, String lastName) {
 			this.firstName = firstName;
 			this.lastName = lastName;
-		}
-
-		@Executable(paramNames = { "street", "zip", "city" })
-		public Address addAddress(String street, String zip, String city) {
-			final Address address = new Address(this, street, zip, city);
-			addresses.add(address);
-			return address;
 		}
 
 		@Override
@@ -86,47 +67,26 @@ public class PersonMultiAddressExample {
 		}
 	}
 
-
-	// --------------------------------------
-
-	public static class Address {
-
-		public Person person;
-		public String street;
-		public String zip;
-		public String city;
-
-		Address(Person person, String street, String zip, String city) {
-			this.person = person;
-			this.street = street;
-			this.zip = zip;
-			this.city = city;
-		}
-
-		@Override
-		public String toString() {
-			return "" + street + ", " + zip + " " + city;
-		}
-	}
-	
-	
 	public static class PersonRelation {
 		public Person from;
 		public Person to;
 		public String forwardRole;
 		public String backwardRole;
-		
+
 		PersonRelation(Person from, Person to, String forwardRole, String backwardRole) {
 			this.from = from;
 			this.to = to;
 			this.forwardRole = forwardRole;
 			this.backwardRole = backwardRole;
+
+			from.relationsOut.add(this);
+			to.relationsIn.add(this);
 		}
-		
+
 		public String getHumanReadableForward() {
 			return "" + from + " " + forwardRole + " " + to;
 		}
-		
+
 		public String getHumanReadableBackward() {
 			return "" + to + " " + backwardRole + " " + from;
 		}
