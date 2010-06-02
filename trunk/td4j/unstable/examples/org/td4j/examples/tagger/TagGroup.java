39ba96ce3a87f5b7a6b3b2f7820f8bf38bcb9de2
miraman <miraman@b6126edc-6d7e-4233-8125-b2df0ea52f23>
2010-06-02 19:49:41 +0000

    JPA sample: tagger

---
+++ trunk/td4j/unstable/examples/org/td4j/examples/tagger/TagGroup.java
@@ -0,0 +1,30 @@
+package org.td4j.examples.tagger;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.td4j.core.reflect.Companion;
+
+@Entity
+@Companion(TagGroupCO.class)
+public class TagGroup {
+	
+	@Id @GeneratedValue private Integer id;
+	
+	@Column(unique=true, nullable=false)
+	public String name;
+	
+	private TagGroup() {}
+	
+	TagGroup(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+}
