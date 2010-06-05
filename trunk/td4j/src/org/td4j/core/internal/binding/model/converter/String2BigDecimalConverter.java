c9e98f6473703e541f3e35c7e93d7322c7ba1002
miraman <miraman@b6126edc-6d7e-4233-8125-b2df0ea52f23>
2010-06-05 18:11:41 +0000

    String converters for byte, short, int, long, float, double and BigDecimal

---
+++ trunk/td4j/src/org/td4j/core/internal/binding/model/converter/String2BigDecimalConverter.java
@@ -0,0 +1,63 @@
+/*********************************************************************
+  This file is part of td4j, see <http://td4j.org/>
+
+  Copyright (C) 2010 Michael Rauch
+
+  td4j is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  td4j is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with td4j.  If not, see <http://www.gnu.org/licenses/>.
+ *********************************************************************/
+
+package org.td4j.core.internal.binding.model.converter;
+
+import java.math.BigDecimal;
+
+public class String2BigDecimalConverter implements IConverter {
+
+	@Override
+	public Class<?> getConversionTargetType() {
+		return BigDecimal.class;
+	}
+	
+  @Override
+  public Object convert(Object from) {
+  	if (from instanceof String) {
+  		final String s = (String)from;
+  		if ( ! s.trim().isEmpty()) return new BigDecimal(s);
+  	}
+  	
+    return null;
+  }
+
+  @Override
+  public boolean canConvert() {
+    return true;
+  }
+
+  @Override
+  public Class<?> getUnconversionTargetType() {
+  	return String.class;
+  }
+  
+  @Override
+  public Object unconvert(Object from) {
+  	if (from instanceof BigDecimal) return from.toString();
+  	
+    return null;
+  }
+
+  @Override
+  public boolean canUnconvert() {
+    return true;
+  }
+
+}
