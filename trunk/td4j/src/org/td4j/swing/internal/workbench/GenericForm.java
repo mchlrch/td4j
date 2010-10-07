e53e281386aae1405bf3980741127925b3351780
miraman <miraman@b6126edc-6d7e-4233-8125-b2df0ea52f23>
2010-10-07 18:20:19 +0000

    move common code to GenericPanelBuilder

--- trunk/td4j/src/org/td4j/swing/internal/workbench/GenericForm.java
+++ trunk/td4j/src/org/td4j/swing/internal/workbench/GenericForm.java
@@ -19,34 +19,14 @@
 
 package org.td4j.swing.internal.workbench;
 
-import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-
-import javax.swing.AbstractButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
 
-import org.td4j.core.binding.model.IndividualDataProxy;
-import org.td4j.core.binding.model.ListDataProxy;
-import org.td4j.core.internal.binding.model.converter.DefaultConverterRepository;
-import org.td4j.core.internal.binding.model.converter.IConverter;
 import org.td4j.core.metamodel.MetaClass;
 import org.td4j.core.metamodel.MetaModel;
 import org.td4j.core.reflect.IndividualProperty;
 import org.td4j.core.reflect.ListProperty;
 import org.td4j.core.tk.ObjectTK;
-import org.td4j.swing.binding.ButtonController;
-import org.td4j.swing.binding.LabelController;
-import org.td4j.swing.binding.LinkController;
-import org.td4j.swing.binding.TableController;
-import org.td4j.swing.binding.TextController;
 import org.td4j.swing.binding.WidgetBuilder;
-import org.td4j.swing.internal.binding.TableControllerFactory;
 import org.td4j.swing.workbench.Editor;
 import org.td4j.swing.workbench.Form;
 import org.td4j.swing.workbench.Navigator;
@@ -63,67 +43,23 @@ public class GenericForm<T> extends Form<T> {
 
 	@Override
 	protected JPanel createForm() {
-		final JPanel panel = new JPanel(new GridBagLayout());
-
 		final MetaModel metaModel = getEditor().getWorkbench().getAppCtx().getMetamodel();
 		final Navigator navigator = getEditor().getWorkbench().getNavigator();
 		
-		// individual plugs
 		final WidgetBuilder<Object> wBuilder = new WidgetBuilder<Object>(getMediator(), metaModel, navigator);
+		final GenericPanelBuilder pBuilder = new GenericPanelBuilder(wBuilder);
 		final MetaClass metaClass = metaModel.getMetaClass(getContextType());
+		
+		// individual plugs
 		for (IndividualProperty individualProperty : metaClass.getIndividualProperties()) {
-			final Class<?> type = individualProperty.getValueType();
-
-			// PEND: das kreieren der widgets (+controller) muss auch pluggable sein,
-			// damit neue controller typen unterstütz werden können
-
-			// PEND: fix this, code duplication with IndividualDataContainer, InvokationParameterDialog
-			// PEND: fix this, temporary only conversion to String supported !!
-		      final Class<?> fromType = type;
-		      final Class<?> toType = String.class;
-		      final IConverter converter = DefaultConverterRepository.INSTANCE.getConverter(fromType, toType);
-			final boolean convertableToString = converter != null;
-			
-			final JLabel label = new JLabel();
-			panel.add(label, new GridBagConstraints(0, - 1, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
-			
-			final IndividualDataProxy individualProxy = new IndividualDataProxy(individualProperty, individualProperty.getName(), converter);
-			getMediator().addContextSocket(individualProxy);
-			if (type == Boolean.class || type == boolean.class) {
-				final ButtonController btnController = wBuilder.caption(label).button().bind(individualProxy);
-				final AbstractButton btn = btnController.getWidget();
-				panel.add(btn, new GridBagConstraints(1, - 1, 1, 1, 0.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
-			} else if (type == String.class || convertableToString) {
-				final TextController textController = wBuilder.caption(label).text().bind(individualProxy);
-				final JTextField text = textController.getWidget();
-				panel.add(text, new GridBagConstraints(1, - 1, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
-			} else if (wBuilder.getNavigator().isTypeNavigatable(type)) {
-				final LinkController linkController = wBuilder.caption(label).link().bind(individualProxy);
-				final JLabel link = linkController.getWidget();
-				panel.add(link, new GridBagConstraints(1, - 1, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
-			} else {
-			  final LabelController<JLabel> labelController = wBuilder.caption(label).label().bind(individualProxy);
-              final JLabel valueLabel = labelController.getWidget();
-              panel.add(valueLabel, new GridBagConstraints(1, - 1, 1, 1, 1.0, 0.0, GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL, new Insets(5, 5, 5, 5), 0, 0));
-			}
+			pBuilder.addIndividualProperty(individualProperty);
 		}
 
 		// list plugs
 		for (ListProperty listProperty : metaClass.getListProperties()) {
-			final JLabel label = new JLabel();
-			panel.add(label, new GridBagConstraints(0, - 1, 1, 1, 0.0, 0.0, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0));
-
-			final ListDataProxy listProxy = new ListDataProxy(listProperty, listProperty.getName(), listProperty.getNestedProperties());
-			getMediator().addContextSocket(listProxy);
-			
-			final TableControllerFactory tableCtrlFactory = wBuilder.caption(label).table();
-			final TableController tableController = tableCtrlFactory.bind(listProxy);
-			final JTable table = tableController.getWidget();
-			final JScrollPane scrollPane = new JScrollPane(table);
-			scrollPane.setPreferredSize(new Dimension(100, 100));
-			panel.add(scrollPane, new GridBagConstraints(1, - 1, 1, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(5, 5, 5, 5), 0, 0));
+			pBuilder.addListProperty(listProperty);
 		}
 
-		return panel;
+		return pBuilder.getPanel();
 	}
 }
