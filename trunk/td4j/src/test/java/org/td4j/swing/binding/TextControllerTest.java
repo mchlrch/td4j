3020a78215d7b0fda05d375fd686a7bc634fd925
miraman <miraman@b6126edc-6d7e-4233-8125-b2df0ea52f23>
2010-12-30 15:36:35 +0000

    created TextControllerTest

---
+++ trunk/td4j/src/test/java/org/td4j/swing/binding/TextControllerTest.java
@@ -0,0 +1,160 @@
+/*********************************************************************
+  This file is part of td4j, see <http://td4j.org/>
+
+  Copyright (C) 2010 Michael Rauch
+
+  td4j is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  td4j is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with td4j.  If not, see <http://www.gnu.org/licenses/>.
+ *********************************************************************/
+
+package org.td4j.swing.binding;
+
+import java.lang.reflect.Method;
+
+import javax.swing.JTextField;
+
+import org.td4j.core.binding.model.IndividualDataProxy;
+import org.td4j.core.internal.binding.model.IndividualMethodConnector;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import ch.miranet.commons.ExceptionTK;
+import ch.miranet.commons.ObjectTK;
+
+public class TextControllerTest {
+	
+  // connector is stateless, so we don't need to recreate it for every testcase 
+	private IndividualMethodConnector dataConnector;
+
+	
+	
+	@Test(dataProvider = "harness")
+	public void testShowInitialModelValueInWidget(final TestHarness harness) {
+		assertTextFieldContent(DataContainer.INITIAL_VALUE, harness);
+	}
+	
+	@Test(dataProvider = "harness")
+	public void testShowChangedModelValueInWidget(final TestHarness harness) {
+		harness.dataContainer.setValue(DataContainer.ACCEPTED_VALUE);
+		harness.dataProxy.refreshFromContext();
+		
+		assertTextFieldContent(DataContainer.ACCEPTED_VALUE, harness);
+	}	
+	
+	@Test(dataProvider = "harness")
+	public void testWriteWidgetValueToModel(final TestHarness harness) {
+		harness.textField.setText(DataContainer.ACCEPTED_VALUE);
+		harness.textController.updateModel();
+		
+		assertTextFieldContent(DataContainer.ACCEPTED_VALUE, harness);
+		assertDataContainerContent(DataContainer.ACCEPTED_VALUE, harness);
+	}
+	
+	@Test(dataProvider = "harness", expectedExceptions = { IllegalArgumentException.class })
+	public void testWriteRejectedWidgetValueToModelAndRecover(final TestHarness harness) throws Throwable {
+		harness.textField.setText(DataContainer.REJECTED_VALUE);
+		
+		try {
+			harness.textController.updateModel();
+		} catch (Exception ex) {			
+			assertTextFieldContent(DataContainer.INITIAL_VALUE, harness);
+			assertDataContainerContent(DataContainer.INITIAL_VALUE, harness);
+			
+			throw ExceptionTK.unwrap(ex);
+		}		
+	}
+
+	private void assertTextFieldContent(final String expectedContent, final TestHarness harness) {
+		assert harness.textField.getText().equals(expectedContent);
+	}
+	
+	private void assertDataContainerContent(final String expectedContent, final TestHarness harness) {
+		assert harness.dataContainer.getValue().equals(expectedContent);
+	}
+	
+	
+	@DataProvider(name = "harness")
+	public Object[][] createHarness() {		
+		if (dataConnector == null) {
+			dataConnector = createDataConnector();
+		}			
+		final IndividualDataProxy dataProxy = new IndividualDataProxy(dataConnector, "value");
+		final DataContainer dataContainer = new DataContainer(); 
+		dataProxy.setContext(dataContainer);
+		
+		final JTextField textField = new JTextField();
+		final TestTextController textController = new TestTextController(textField, dataProxy);
+		
+		final TestHarness harness = new TestHarness(dataContainer, dataProxy, textController, textField);
+		return new Object[][] { { harness } };
+	}
+	
+	private IndividualMethodConnector createDataConnector() {
+		try {
+			final Method getter = DataContainer.class.getMethod("getValue");
+			final Method setter = DataContainer.class.getMethod("setValue", String.class);
+			
+			final IndividualMethodConnector dataConnector = new IndividualMethodConnector(DataContainer.class, getter, setter);
+			return dataConnector;
+			
+		} catch (NoSuchMethodException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+	
+	// -------------------------------------------
+	
+	public static class TestHarness {
+		public final DataContainer       dataContainer;
+		public final IndividualDataProxy dataProxy;
+		public final TestTextController  textController;
+		public final JTextField          textField;
+		
+		private TestHarness(DataContainer dataContainer, IndividualDataProxy dataProxy, TestTextController textController, JTextField textField) {
+			this.dataContainer  = ObjectTK.enforceNotNull(dataContainer,  "dataContainer");
+			this.dataProxy      = ObjectTK.enforceNotNull(dataProxy,      "dataProxy");
+			this.textController = ObjectTK.enforceNotNull(textController, "textController");
+			this.textField      = ObjectTK.enforceNotNull(textField,      "textField");
+		}
+	}
+	
+	public static class DataContainer {
+		public static final String INITIAL_VALUE  = "initial";
+		public static final String ACCEPTED_VALUE = "accept";
+		public static final String REJECTED_VALUE = "reject";
+		
+		private String value = INITIAL_VALUE;
+		
+		public String getValue() {
+			return value;
+		}
+		
+		public void setValue(String s)  {
+			if (REJECTED_VALUE.equals(s)) throw new IllegalArgumentException(REJECTED_VALUE);
+			
+			this.value = s;
+		}
+	}
+	
+	public static class TestTextController extends TextController {
+		private TestTextController(JTextField widget, IndividualDataProxy proxy) {
+			super(widget, proxy);
+		}
+		
+		@Override  // open access
+		public void updateModel() {
+			super.updateModel();
+		}
+	}
+
+}
